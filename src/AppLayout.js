0e1c72f44acbe41bc92b7420dc2b35267ece2019
lmpko <lmpkoo@gmail.com>
2023-05-16 07:53:21 +0200

    linecomponent deleted, and elements added t oform as button with css and active states

--- src/AppLayout.js
+++ src/AppLayout.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import DateFormCompononet from './DateFormCompononet'
-import LineComponent from "./LineComponent"
 import ShowComponent from "./ShowComponent"
 
 const AppLayout = () => {
   return (
     <>
         <DateFormCompononet/>
-        <LineComponent />
         <ShowComponent />
     </>
   )
--- src/DateFormCompononet.js
+++ src/DateFormCompononet.js
@@ -1,25 +1,37 @@
 import React from 'react'
+import arrowIMG from "/assets/images/icon-arrow.svg"
 
 const DateFormCompononet = () => {
   return (
     <form className='date-form'>
       <label for="day">
         day
-        <input type="number" name="day" value="" 
-          id='day' placeholder='DD'/>
+        <input type="number" name="day" 
+          id='day' placeholder='DD'
+          min="1" max="31" step="1"/>
       </label>
 
       <label for="month">
         month
-        <input type="number" name="month" value="" 
-          id='month' placeholder='MM'/>
+        <input type="number" name="month" 
+          id='month' placeholder='MM'
+          min="1" max="12" step="1"/>
       </label>
 
       <label for="year">
         year
-        <input type="number" name="year" value=""
-           id='year' placeholder='YYYY'/>
+        <input type="number" name="year" 
+           id='year' placeholder='YYYY'
+           step="1" min="0"/>
       </label>
+
+      <div className='line-with-img'>
+      <hr/>
+      <button>
+        <img src={arrowIMG} alt="arrow-icon"/>
+      </button>
+        
+    </div>
     </form>
   )
 }
